52df066f99f73d4c4d1429e8aa11d8e6882e01b4
Anton Kuzmin <plasmatrip@gmail.com>
2024-03-17 20:51:29 +0300

    feat: implement API testing using testify

---
+++ main_test.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var cafeList = map[string][]string{
+	"moscow": []string{"Мир кофе", "Сладкоежка", "Кофе и завтраки", "Сытый студент"},
+}
+
+func mainHandle(w http.ResponseWriter, req *http.Request) {
+	countStr := req.URL.Query().Get("count")
+	if countStr == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("count missing"))
+		return
+	}
+
+	count, err := strconv.Atoi(countStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong count value"))
+		return
+	}
+
+	city := req.URL.Query().Get("city")
+
+	cafe, ok := cafeList[city]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("wrong city value"))
+		return
+	}
+
+	if count > len(cafe) {
+		count = len(cafe)
+	}
+
+	answer := strings.Join(cafe[:count], ",")
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(answer))
+}
+
+func TestMainHandlerWhenOk(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//проверка, что сервер вернул код ответа 200
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
+}
+
+func TestMainHandlerWhenWrongCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=2&city=murmansk", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//проверка, что сервер вернул код ответа 400
+	//если ответ не Bad Requset, то дальше проверять нечего
+	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
+
+	//ожидаемое сообщение от сервера
+	expected := "wrong city value"
+	//проверка, что при неправильном городе сервер вернул в теле ответа сообщение "wrong city value"
+	assert.Equal(t, expected, responseRecorder.Body.String())
+}
+
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	totalCount := 4
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=5", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	//проверка, что сервер вернул код ответа 200
+	//если ответ не ОК, то дальше проверять нечего
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+
+	//преобразуем ответ сервера с кафе из строки в слайс
+	listOfCafe := strings.Split(responseRecorder.Body.String(), ",")
+	//проряем, что количество кафе равно ожидаемому
+	assert.Len(t, listOfCafe, totalCount)
+}
